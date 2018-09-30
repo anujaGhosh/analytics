338496af615c8fa50737dd6f4ad27be432349f8b
anujaGhosh <anughosh123@outlook.com>
2018-09-30 11:36:05 +0530

    30th

--- Dplyr_Analysis.R
+++ Dplyr_Analysis.R
@@ -4,3 +4,7 @@ dplyr::filter(mtcars,mpg > 25 & vs==1)#::is used for scope
 ?mtcars
 
 mtcars%>% filter(mpg>25 & vs==1) #same as line 3
+mtcars%>% filter(mpg>25 & vs==1) %>% arrange(wt) %>% count()#next function on previous stage and not on entire dataset
+mtcars%>% filter(mpg>25 & vs==1) %>% arrange(wt) %>% summarize(n()) #count the number of rows using n() function
+mtcars%>% group_by(am) %>% summarize(mean(mpg)) #segregate into groups then find mean
+mtcars%>%group_by(gear) %>% summarize(mean(wt))
